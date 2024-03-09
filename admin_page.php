593320dbf5bc035bd89c3d833f6add187408a591
FaizAhmad0 <118246870+FaizAhmad0@users.noreply.github.com>
2024-03-09 11:57:11 +0530

    added user and admin dashboard

--- admin_page.php
+++ admin_page.php
@@ -36,11 +36,11 @@ if(!isset($_SESSION['admin_name'])){
    <div class="mainBox">
       <div class="userCompo">
          <div class="content">
-         <h5 class="wlcm">Welcome <span><?php echo $_SESSION['admin_name'] ?></span></h5>
+         <h6 class="wlcm">Welcome <span><?php echo $_SESSION['admin_name'] ?></span></h6>
          <div class="d-grid gap-1">
             <a href="index.html" class="btn btn-outline-primary">Home</a>
-            <button class="btn btn-outline-primary" type="button">PAYMENTS</button>
-            <button class="btn btn-outline-primary" type="button">GIFT CARDS</button>
+            <button class="btn btn-outline-primary" type="button">Offers</button>
+            <button class="btn btn-outline-primary" type="button">Tools</button>
             <button class="btn btn-outline-primary" type="button">MY COUPONS</button>
             <button class="btn btn-outline-primary" type="button">MY WISH LIST</button>
             <a href="logout.php" class="btn btn-outline-danger"><img src="assets/logout.jpg" alt="">  logout</a>
@@ -53,6 +53,40 @@ if(!isset($_SESSION['admin_name'])){
             <a class="btn btn-outline-success" href="index.html">Click here to add car</a>
          </div>
          
+         <!-- activity tracker       -->
+         <div class="activity-tracker">
+        <h1>Activity Tracking</h1>
+        <div class="activity-list">
+            <div class="activity">
+                <p class="activity-type">Sold out</p>
+                <p class="activity-duration">10</p>
+            </div>
+            <div class="activity">
+                <p class="activity-type">Available</p>
+                <p class="activity-duration">45</p>
+            </div>
+            <div class="activity">
+                <p class="activity-type">Upcomming Bookings</p>
+                <p class="activity-duration">50</p>
+            </div>
+            <!-- Add more activities here -->
+        </div>
+    </div>
+         <!-- end of activity trackeer -->
+
+
+      </div>
+      <div class="bookings">
+            <h4 class="upcbooking btn btn-outline-primary">Upcomming bookings</h4>            
+            <ul class="upcItem"><li>Ferrari</li></ul>
+            <ul class="upcItem"><li>McLaren</li></ul>
+            <ul class="upcItem"><li>Bugatti</li></ul>
+            <ul class="upcItem"><li>Lamborgini</li></ul>
+            <ul class="upcItem"><li>Mecerati</li></ul>
+            <ul class="upcItem"><li>Mecerati</li></ul>
+            <ul class="upcItem"><li>Aston martin</li></ul>
+            <ul class="upcItem"><li>Jaguar</li></ul>
+            
       </div>
    </div>
 </body>
